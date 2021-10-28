81d21dbddc1a229dc625acb8ee1cc7ee8355d86a
ginacitra98 <nopitasariginacitra@gmail.com>
2021-10-28 08:36:18 +0100

    tugas5

--- application/views/tampil_data.php
+++ application/views/tampil_data.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tampilkan Data</title>
+	<title>Tampil Data</title>
 </head>
-<body>
-    <center><hl>Data Mahasiswa</hl></center>
-    <table style="margin:20px auto;" border="1">
-        <tr>
-            <th>No</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Pekerjaan</th>
-            <th>Foto</th>
-            <th>Action</th>
-        </tr>
-        <?php
-        $no= 1;
-        foreach($mahasiswa as $u) {
-        ?>
-        <tr>
-            <td><?php echo $no++ ?></td>
-            <td><?php echo $u->nim ?></td>
-            <td><?php echo $u->nama ?></td>
-            <td><?php echo $u->alamat ?></td>
-            <td><?php echo $u->pekerjaan ?></td>
-            <td><img scr="<?php echo base_url();?><php echo $u->foto ?>" width="50" height="50"></td>
-            <td>
-                    <?php echo anchor ('kampus/edit/'.$u->id,'Edit'); ?>
-                    <?php echo anchor ('kampus/hapus/'.$u->id,'Hapus');?>
-            </td>
-        </tr>
-        <?php } ?>
-    </table>
-    <center><?Php echo anchor ('kampus/tambah','<input type=button value=\'Tambah Data\'>'); ?></center>
+</body>
+	<center><h1>Data Mahasiswa</h1></center>
+	<table style="margin:20px auto;" border="1">
+		<tr>
+			<th>No</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Alamat</th>
+			<th>Pekerjaan</th>
+			<th>Foto</th>
+			<th>Action</th>
+		</tr>
+		<?php
+		$no = 1;
+		foreach($mahasiswa as $u) {
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $u->nim ?></td>
+			<td><?php echo $u->nama ?></td>
+			<td><?php echo $u->alamat ?></td>
+			<td><?php echo $u->pekerjaan ?></td>
+			<td><img src="<?php echo base_url();?><?php echo $u->foto ?>" width="50" height="50"></td>
+			<td>
+				<?php echo anchor('kampus/edit/'.$u->id, 'Edit'); ?>
+				<?php echo anchor('kampus/hapus/'.$u->id, 'Hapus'); ?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+	<center><?php echo anchor('kampus/tambah','<input type=button value=\'Tambah Data\'>'); ?></center>
 </body>
 </html>
+© 2021 GitHub, Inc.
+Terms
